13e3d234104402a374772cfaa11cc0bb1e167abe
qrwells <qirui.wang@moegi.waseda.jp>
2022-05-01 18:18:21 +0900

    Implemented fcfs schduler.

--- src/arch/risc-v/kernel/scheduler/fcfs_scheduler.c
+++ src/arch/risc-v/kernel/scheduler/fcfs_scheduler.c
@@ -2,12 +2,51 @@
 #include "constants.h"
 #include "defs.h"
 
-// void scheduler_init() {}
+typedef struct Node {
+  int tid;
+} tinfo;
 
-// void scheduler_push(int tid) {}
+struct {
+  tinfo threads[MAX_THREAD];
+  int front;
+  int rear;
+  int size;
+  int current;
+} fcfs_scheduler;
 
-// int scheduler_pop() {}
+void scheduler_init() {
+  fcfs_scheduler.current = -1;
+  fcfs_scheduler.front = -1;
+  fcfs_scheduler.rear = -1;
+  fcfs_scheduler.size = 0;
+}
 
-// int scheduler_tick() {}
+void scheduler_push(int tid) {
+  if (fcfs_scheduler.size >= MAX_THREAD)
+    panic("queue is full!\n");
 
-// void scheduler_exit(int tid) {}
+  tinfo t = {tid};
+  fcfs_scheduler.rear++;
+  fcfs_scheduler.rear %= MAX_THREAD;
+  fcfs_scheduler.size++;
+  fcfs_scheduler.threads[fcfs_scheduler.rear] = t;
+}
+
+int scheduler_pop() {
+  if (fcfs_scheduler.size == 0)
+    return -1;
+
+  fcfs_scheduler.front++;
+  fcfs_scheduler.front %= MAX_THREAD;
+  fcfs_scheduler.size--;
+  int tid = fcfs_scheduler.threads[fcfs_scheduler.front].tid;
+  fcfs_scheduler.current = tid;
+  return tid;
+}
+
+int scheduler_tick() { return 0; }
+
+void scheduler_exit(int tid) {
+  if (fcfs_scheduler.current == tid)
+    fcfs_scheduler.current = -1;
+}
